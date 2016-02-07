faf0ec53d2071f4113d101c7d18708f4f0cb7ef8
北䑓如法 <algebraicallyClosedField@gmail.com>
2016-02-07 14:30:56 +0900

    Make the initial commit

---
+++ TabTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TabTest
+//
+//  Created by 北䑓 如法 on 16/2/7.
+//  Copyright © 2016年 北䑓 如法. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TabTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TabTest
+//
+//  Created by 北䑓 如法 on 16/2/7.
+//  Copyright © 2016年 北䑓 如法. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
